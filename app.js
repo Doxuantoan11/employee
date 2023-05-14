035e3cf668abcb5c13baac71bda1d974eaa47b76
Do toan <doxuantoan11@gmail.com>
2023-05-14 23:48:18 +0700

    Authentication and Authorization

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var methodOverride = require('method-override')
 var moment = require('moment');
+const jwt = require('jsonwebtoken');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const LocalStrategy = require('passport-local').Strategy;
+const passport = require('passport');
+const flash = require('connect-flash');
 
 var app = express();
 app.use(methodOverride('_method'))
@@ -27,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,18 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var MongoClient = require('mongodb').MongoClient;
 var moment = require('moment');
-
 //upload file
 var multer = require('multer');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const LocalStrategy = require('passport-local').Strategy;
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('connect-flash');
 
-
+const secret = "ct24";
+router.use(flash());
 
 mongoose.connect('mongodb://127.0.0.1:27017/canbo', {useNewUrlParser: true, useNewUrlParser: true,  useUnifiedTopology: true});
 var db = mongoose.connection;
@@ -64,41 +71,185 @@ var userSchema =new mongoose.Schema({
   password: {
     type: String,
   },
+  roleID: {
+    type: Number,
+  },
 });
 
 var user = mongoose.model('users', userSchema);
-/* GET home page. */
+
+var roleSchema =new mongoose.Schema({
+  roldID: {
+    type: Number,
+  },
+  name: {
+    type: String,
+  },
+});
+
+var role = mongoose.model('roles', roleSchema);
+
+var user_info ="";
+router.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
+}));
+router.use(passport.initialize());
+router.use(passport.session());
+
+passport.use(new LocalStrategy({
+  usernameField: 'username'
+}, async (username, password, done) => {
+  try {
+    const myUser  = await user.findOne({ username });
+    user_info = username;
+    if (!myUser ) {
+      return done(null, false, { message: 'Incorrect username or password.' });
+    }
+    const isMatch = password === myUser.password;
+    if (!isMatch) {
+      return done(null, false, { message: 'Incorrect username or password.' });
+    }
+    return done(null, myUser );
+  } catch (err) {
+    return done(err);
+  }
+}));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const myuser1 = await user.findById(id);
+    done(null, myuser1);
+  } catch (err) {
+    done(err);
+  }
+});
+// Configure session middleware
+
 router.get('/', function(req, res) {
   test.find({}, (error, data) => {
-    console.log('danh sach canbo: ', data);
-       res.render('index', { test:data });
+    if (req.isAuthenticated()) {
+      res.render('index', { test:data, user: req.user });
+    } else {
+      res.redirect('/login');
+    }
   });
 });
 //form-login
 router.get('/login', function(req, res) {
   res.render('login', {});
 });
-// kiem tra login
-router.post('/login', async(req, res)=> {
-    try {
-      const check = await user.findOne({username: req.body.username})
-
-      if(check.password === req.body.password){
-        res.redirect('/')
-      }
-      else {
-        res.send("wrong password")
-      }
-    }
-    catch {
-      res.send("wrong details")
-    }
-
-    
+router.post('/login', passport.authenticate('local', {
+  failureFlash: 'Incorrect username or password.',
+  failureRedirect: '/login'
+}), (req, res) => {
+  res.redirect('/');
 });
 
+router.get('/logout', (req, res) => {
+  req.logout((err) => {
+    if (err) {
+      console.error(err);
+    }
+    res.redirect('/login');
+  });
+});
+
+// router.post('/login', async (req, res) => {
+//   try {
+//     user_info = req.body.username;
+//     console.log(user_info);
+//     const { username, password } = req.body;
+//     const user = await user.findOne({ username });
+//     if (!user) {
+//       return res.status(400).json({ message: 'User not found' });
+//     }
+//     const isMatch = await bcrypt.compare(password, user.password);
+//     if (!isMatch) {
+//       return res.status(400).json({ message: 'Invalid credentials' });
+//     }
+//     const token = jwt.sign({ id: user._id }, secret);
+//     res.json({ token });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ message: 'Server error' });
+//   }
+// });
+
+
+// router.post('/signup', async (req, res) => {
+//   try {
+//     const { username, password } = req.body;
+//     let user = await user.findOne({ username });
+//     if (user) {
+//       return res.status(400).json({ message: 'User already exists' });
+//     }
+//     user = new user({
+//       username,
+//       password
+//     });
+//     // const salt = await bcrypt.genSalt(10);
+//     // user.password = await bcrypt.hash(password, salt);
+//     await user.save();
+//     const token = jwt.sign({ id: user._id }, secret);
+//     res.json({ token });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ message: 'Server error' });
+//   }
+// });
+
+
+// kiem tra login
+
+// router.post('/login', async(req, res)=> {
+//   try {
+//     const check = await user.findOne({username: req.body.username})
+//     if(check.password === req.body.password){
+//       user_info = check;
+//       const token = jwt.sign({ _id: user._id }, secret);
+//       res.cookie('token', token, { httpOnly: true, path: '/' });
+//       res.redirect('/')
+//       }
+//       else {
+//         res.send("wrong password")
+//       }
+//     }
+//     catch {
+//       res.send("wrong details")
+//     }
+// });
+
+// router.post('/logout', (req, res) => {
+//   res.clearCookie('token');
+//   console.log('Logout successful');
+//   res.json({ message: 'Logout successful' });
+// });
+// router.get('/logout', (req, res) => {
+//   res.clearCookie('token', { domain: 'http://localhost:3001/', path: '/login' });
+//   if (req.cookies.token) {
+//     console.log('yes cookies token is exist');
+//   } else {
+//     console.log('no cookies token is  not exist');
+//   }
+//   // console.log('Logout successful');
+//   // res.json({ message: 'Logout successful' });
+//   res.redirect('/login');
+// });
+
+/* GET home page. */
 
 //form-sign  giao dien trang dang ky
+// router.get('/signup', function(req, res) {
+//   res.render('signup', {});
+// });
+
 router.get('/signup', function(req, res) {
   res.render('signup', {});
 });
@@ -188,7 +339,11 @@ router.get('/form-delete/:id', function(req, res) {
 //tim kiem
 router.get('/search', function(req, res) {
   test.find({}, (error, data) => {
-       res.render('search', { test:data});
+    if (req.isAuthenticated()) {
+      res.render('index', { test:data, user: req.user });
+    } else {
+      res.send(' you have to be Manger to access this page lick here to login page');
+    }
   });
 });
 
